eec35a7c494a5a1f2635c2ca29807960b74ef933
Mudassar <mudassar@Mudassar-MacBook-Pro.local>
2023-10-10 11:45:58 +0500

    app completed

--- src/config/ethers.js
+++ src/config/ethers.js
@@ -6,6 +6,7 @@ export const AuthContractAddress = "0x0E447f21Ef5f66E4e6Bf9529C49813AfB4d6905F";
 export const TransferAmountAddress = "0xdC410B47f671f94D68692E33d0eCCBFA5ADCff57";
 export const DepositAmountAddress = "0xD1C98A9F67EeD2579288e477775FAcDb92154d79";
 export const OceanTokenAddress="0xdb718024eBcb8217715F6A9531c1B8b8B2eC3523"
+export const NFTAddress="0x6Eff936197ad397aE37Fc229B1877c60D0937cd0"
 // export const OceanTokenAddress="0xFd738477Af8ecd8eA25662f8Bf004D04a0eA34E7"
 ///////////////////////////////contract's ABI/////////////////////////////////
 
@@ -701,6 +702,452 @@ export const OceanTokenABI=[
         "type": "bool"
       }
     ],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  }
+]
+
+export const NFTABI=[
+  {
+    "inputs": [],
+    "stateMutability": "nonpayable",
+    "type": "constructor"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": true,
+        "internalType": "address",
+        "name": "owner",
+        "type": "address"
+      },
+      {
+        "indexed": true,
+        "internalType": "address",
+        "name": "approved",
+        "type": "address"
+      },
+      {
+        "indexed": true,
+        "internalType": "uint256",
+        "name": "tokenId",
+        "type": "uint256"
+      }
+    ],
+    "name": "Approval",
+    "type": "event"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": true,
+        "internalType": "address",
+        "name": "owner",
+        "type": "address"
+      },
+      {
+        "indexed": true,
+        "internalType": "address",
+        "name": "operator",
+        "type": "address"
+      },
+      {
+        "indexed": false,
+        "internalType": "bool",
+        "name": "approved",
+        "type": "bool"
+      }
+    ],
+    "name": "ApprovalForAll",
+    "type": "event"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": false,
+        "internalType": "uint256",
+        "name": "_fromTokenId",
+        "type": "uint256"
+      },
+      {
+        "indexed": false,
+        "internalType": "uint256",
+        "name": "_toTokenId",
+        "type": "uint256"
+      }
+    ],
+    "name": "BatchMetadataUpdate",
+    "type": "event"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": false,
+        "internalType": "uint256",
+        "name": "_tokenId",
+        "type": "uint256"
+      }
+    ],
+    "name": "MetadataUpdate",
+    "type": "event"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": true,
+        "internalType": "address",
+        "name": "previousOwner",
+        "type": "address"
+      },
+      {
+        "indexed": true,
+        "internalType": "address",
+        "name": "newOwner",
+        "type": "address"
+      }
+    ],
+    "name": "OwnershipTransferred",
+    "type": "event"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": true,
+        "internalType": "address",
+        "name": "from",
+        "type": "address"
+      },
+      {
+        "indexed": true,
+        "internalType": "address",
+        "name": "to",
+        "type": "address"
+      },
+      {
+        "indexed": true,
+        "internalType": "uint256",
+        "name": "tokenId",
+        "type": "uint256"
+      }
+    ],
+    "name": "Transfer",
+    "type": "event"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "to",
+        "type": "address"
+      },
+      {
+        "internalType": "uint256",
+        "name": "tokenId",
+        "type": "uint256"
+      }
+    ],
+    "name": "approve",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "owner",
+        "type": "address"
+      }
+    ],
+    "name": "balanceOf",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "tokenId",
+        "type": "uint256"
+      }
+    ],
+    "name": "getApproved",
+    "outputs": [
+      {
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "owner",
+        "type": "address"
+      },
+      {
+        "internalType": "address",
+        "name": "operator",
+        "type": "address"
+      }
+    ],
+    "name": "isApprovedForAll",
+    "outputs": [
+      {
+        "internalType": "bool",
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "recipient",
+        "type": "address"
+      },
+      {
+        "internalType": "string",
+        "name": "tokenURI",
+        "type": "string"
+      }
+    ],
+    "name": "mintNFT",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "name",
+    "outputs": [
+      {
+        "internalType": "string",
+        "name": "",
+        "type": "string"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "owner",
+    "outputs": [
+      {
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "tokenId",
+        "type": "uint256"
+      }
+    ],
+    "name": "ownerOf",
+    "outputs": [
+      {
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "renounceOwnership",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "from",
+        "type": "address"
+      },
+      {
+        "internalType": "address",
+        "name": "to",
+        "type": "address"
+      },
+      {
+        "internalType": "uint256",
+        "name": "tokenId",
+        "type": "uint256"
+      }
+    ],
+    "name": "safeTransferFrom",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "from",
+        "type": "address"
+      },
+      {
+        "internalType": "address",
+        "name": "to",
+        "type": "address"
+      },
+      {
+        "internalType": "uint256",
+        "name": "tokenId",
+        "type": "uint256"
+      },
+      {
+        "internalType": "bytes",
+        "name": "data",
+        "type": "bytes"
+      }
+    ],
+    "name": "safeTransferFrom",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "operator",
+        "type": "address"
+      },
+      {
+        "internalType": "bool",
+        "name": "approved",
+        "type": "bool"
+      }
+    ],
+    "name": "setApprovalForAll",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "bytes4",
+        "name": "interfaceId",
+        "type": "bytes4"
+      }
+    ],
+    "name": "supportsInterface",
+    "outputs": [
+      {
+        "internalType": "bool",
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "symbol",
+    "outputs": [
+      {
+        "internalType": "string",
+        "name": "",
+        "type": "string"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "tokenId",
+        "type": "uint256"
+      }
+    ],
+    "name": "tokenURI",
+    "outputs": [
+      {
+        "internalType": "string",
+        "name": "",
+        "type": "string"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "from",
+        "type": "address"
+      },
+      {
+        "internalType": "address",
+        "name": "to",
+        "type": "address"
+      },
+      {
+        "internalType": "uint256",
+        "name": "tokenId",
+        "type": "uint256"
+      }
+    ],
+    "name": "transferFrom",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "newOwner",
+        "type": "address"
+      }
+    ],
+    "name": "transferOwnership",
+    "outputs": [],
     "stateMutability": "nonpayable",
     "type": "function"
   }
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import useDashboard from "../hooks/useDashboard";
 import { ethers } from "ethers";
+import { Link } from "react-router-dom";
 const Dashboard = () => {
   const {
     register,
@@ -40,157 +41,175 @@ const Dashboard = () => {
   };
   return (
     <>
-    {window.ethereum?<div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-r from-blue-500 to-purple-500">
-    
-      <p className="mb-6 text-[60px] text-gray-800 font-bold">Bank DApp</p>
-      <div className="  border-b-2 pb-5 flex gap-5">
-        <button
-          onClick={() => {
-            setModel("Transfer");
-          }}
-          className="w-full bg-purple-600 py-3 px-4 whitespace-nowrap text-white rounded-md hover:bg-purple-700"
-        >
-          Transfer Amount
-        </button>
-        <button
-          onClick={() => {
-            setModel("Deposit");
-          }}
-          className="w-full bg-purple-600 py-3 px-4 whitespace-nowrap text-white rounded-md hover:bg-purple-700"
-        >
-          Deposit Amount
-        </button>
-        <button
-          onClick={() => {
-            setModel("Withdraw");
-          }}
-          className="w-full bg-purple-600 py-3 px-4 whitespace-nowrap text-white rounded-md hover:bg-purple-700"
-        >
-          Withdraw Amount
-        </button>
-      </div>
-      {model !== "" ? (
-        <div className="bg-white p-6 rounded-lg shadow-lg mt-4 w-96">
-          <h1 className="text-2xl font-semibold mb-4 text-center text-purple-800">
-            {model} Form
-          </h1>
-          <form onSubmit={handleSubmit(onSubmit)}>
-            {model === "Transfer" ? (
-              <div className="mb-4">
-                <label
-                  htmlFor="accountNumber"
-                  className="block text-sm font-medium text-gray-700"
-                >
-                  Account Number
-                </label>
-                <input
-                  type="text"
-                  id="accountNumber"
-                  {...register("accountNumber", { required: true })}
-                  className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                />
-                {errors.accountNumber && (
-                  <p className="text-red-600 text-sm mt-1">
-                    Account Number is required
-                  </p>
-                )}
-              </div>
-            ) : null}
-            <div className="mb-4">
-              <label
-                htmlFor="amount"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Amount
-              </label>
-              <input
-                type="number"
-                id="amount"
-                {...register("amount", { required: true })}
-                className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-              />
-              {errors.amount && (
-                <p className="text-red-600 text-sm mt-1">Amount is required</p>
-              )}
-            </div>
+      {window.ethereum ? (
+        <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-r from-blue-500 to-purple-500">
+         
+          <p className="mb-6 text-[60px] text-gray-800 font-bold">Bank DApp</p>
+         <Link to={"/token"}>
+          <button className="w-full mb-3 bg-purple-600 py-3 px-4 whitespace-nowrap text-white rounded-md hover:bg-purple-700">
+           ERC20 Token
+          </button>
+         </Link>
+          <div className="  border-b-2 pb-5 flex gap-5">
             <button
-              type="submit"
-              className="w-full bg-purple-600 py-2 px-4 text-white rounded-md hover:bg-purple-700"
+              onClick={() => {
+                setModel("Transfer");
+              }}
+              className="w-full bg-purple-600 py-3 px-4 whitespace-nowrap text-white rounded-md hover:bg-purple-700"
             >
-              {model}
+              Transfer Amount
             </button>
-          </form>
-        </div>
-      ) : null}
-      <div className="flex w-[40%] justify-between  item-center"> 
-        <button
-          onClick={() => {
-            contractBalance();
-          }}
-          className="mt-4 bg-purple-600 py-2 px-4 whitespace-nowrap text-white rounded-md hover:bg-purple-700"
-        >
-          Contract Balance
-        </button>
-        <p className="text-white mt-6">{cntrctBalance}{cntrctBalance?" ETH":null}</p>
-      </div>
-      <div className="flex w-[40%] justify-between item-center"> 
-      
-        <button
-          onClick={() => {
-            accountBalance();
-          }}
-          className="mt-4  bg-purple-600 py-2 px-4 whitespace-nowrap text-white rounded-md hover:bg-purple-700"
-        >
-          Account Balance
-        </button>
-        <p className="text-white mt-6">{acntBalance}{acntBalance?" ETH":null}</p>
-      </div>
-      <div>
-        <button
-          onClick={transectionHistory}
-          className="mt-4  bg-purple-600 py-2 px-4 whitespace-nowrap text-white rounded-md hover:bg-purple-700"
-        >
-          Transaction History
-        </button>
-      </div>
-
-      {history.length > 0 && show ? (
-        history.map((item, index) => (
-          // const amount= ethers.utils.formatEther(history[0].nonce)
-          <div className="bg-white p-6 rounded-lg shadow-lg w-[60%] mt-2">
-            <p>
-              <span className="font-bold text-purple-800">Index:</span>
-              {index}
-            </p>
-
-            <p>
-              <span className="font-bold text-purple-800">From:</span>
-              {item.from}
-            </p>
-            <p>
-              <span className="font-bold text-purple-800">To:</span>
-              {item.to}
-            </p>
-            <p>
-              <span className="font-bold text-purple-800">Amount:</span>
-              {ethers.utils.formatEther(item.amount)}ETH
-            </p>
-            <p>
-              <span className="font-bold text-purple-800">Nonce:</span>
-              {ethers.utils.formatEther(item.nonce)}ETH
-            </p>
-            <p>
-              <span className="font-bold text-purple-800">Gas Fee:</span>
-              {ethers.utils.formatEther(item.gasfee)}ETH
+            <button
+              onClick={() => {
+                setModel("Deposit");
+              }}
+              className="w-full bg-purple-600 py-3 px-4 whitespace-nowrap text-white rounded-md hover:bg-purple-700"
+            >
+              Deposit Amount
+            </button>
+            <button
+              onClick={() => {
+                setModel("Withdraw");
+              }}
+              className="w-full bg-purple-600 py-3 px-4 whitespace-nowrap text-white rounded-md hover:bg-purple-700"
+            >
+              Withdraw Amount
+            </button>
+          </div>
+          {model !== "" ? (
+            <div className="bg-white p-6 rounded-lg shadow-lg mt-4 w-96">
+              <h1 className="text-2xl font-semibold mb-4 text-center text-purple-800">
+                {model} Form
+              </h1>
+              <form onSubmit={handleSubmit(onSubmit)}>
+                {model === "Transfer" ? (
+                  <div className="mb-4">
+                    <label
+                      htmlFor="accountNumber"
+                      className="block text-sm font-medium text-gray-700"
+                    >
+                      Account Number
+                    </label>
+                    <input
+                      type="text"
+                      id="accountNumber"
+                      {...register("accountNumber", { required: true })}
+                      className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                    />
+                    {errors.accountNumber && (
+                      <p className="text-red-600 text-sm mt-1">
+                        Account Number is required
+                      </p>
+                    )}
+                  </div>
+                ) : null}
+                <div className="mb-4">
+                  <label
+                    htmlFor="amount"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    Amount
+                  </label>
+                  <input
+                    type="number"
+                    id="amount"
+                    {...register("amount", { required: true })}
+                    className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                  />
+                  {errors.amount && (
+                    <p className="text-red-600 text-sm mt-1">
+                      Amount is required
+                    </p>
+                  )}
+                </div>
+                <button
+                  type="submit"
+                  className="w-full bg-purple-600 py-2 px-4 text-white rounded-md hover:bg-purple-700"
+                >
+                  {model}
+                </button>
+              </form>
+            </div>
+          ) : null}
+          <div className="flex w-[40%] justify-between  item-center">
+            <button
+              onClick={() => {
+                contractBalance();
+              }}
+              className="mt-4 bg-purple-600 py-2 px-4 whitespace-nowrap text-white rounded-md hover:bg-purple-700"
+            >
+              Contract Balance
+            </button>
+            <p className="text-white mt-6">
+              {cntrctBalance}
+              {cntrctBalance ? " ETH" : null}
             </p>
           </div>
-        ))
-      ) : history.length === 0 && show ? (
-        <p className="text-white">No record Found</p>
-      ) : null}
-      </div>:<p className="flex justify-center items-center h-screen text-[40px] font-medium"> Plx Install MetaMask Firstly</p>}
-      </>
-    
+          <div className="flex w-[40%] justify-between item-center">
+            <button
+              onClick={() => {
+                accountBalance();
+              }}
+              className="mt-4  bg-purple-600 py-2 px-4 whitespace-nowrap text-white rounded-md hover:bg-purple-700"
+            >
+              Account Balance
+            </button>
+            <p className="text-white mt-6">
+              {acntBalance}
+              {acntBalance ? " ETH" : null}
+            </p>
+          </div>
+          <div>
+            <button
+              onClick={transectionHistory}
+              className="mt-4  bg-purple-600 py-2 px-4 whitespace-nowrap text-white rounded-md hover:bg-purple-700"
+            >
+              Transaction History
+            </button>
+          </div>
+
+          {history.length > 0 && show ? (
+            history.map((item, index) => (
+              // const amount= ethers.utils.formatEther(history[0].nonce)
+              <div className="bg-white p-6 rounded-lg shadow-lg w-[60%] mt-2">
+                <p>
+                  <span className="font-bold text-purple-800">Index:</span>
+                  {index}
+                </p>
+
+                <p>
+                  <span className="font-bold text-purple-800">From:</span>
+                  {item.from}
+                </p>
+                <p>
+                  <span className="font-bold text-purple-800">To:</span>
+                  {item.to}
+                </p>
+                <p>
+                  <span className="font-bold text-purple-800">Amount:</span>
+                  {ethers.utils.formatEther(item.amount)}ETH
+                </p>
+                <p>
+                  <span className="font-bold text-purple-800">Nonce:</span>
+                  {ethers.utils.formatEther(item.nonce)}ETH
+                </p>
+                <p>
+                  <span className="font-bold text-purple-800">Gas Fee:</span>
+                  {ethers.utils.formatEther(item.gasfee)}ETH
+                </p>
+              </div>
+            ))
+          ) : history.length === 0 && show ? (
+            <p className="text-white">No record Found</p>
+          ) : null}
+        </div>
+      ) : (
+        <p className="flex justify-center items-center h-screen text-[40px] font-medium">
+          {" "}
+          Plx Install MetaMask Firstly
+        </p>
+      )}
+    </>
   );
 };
 
